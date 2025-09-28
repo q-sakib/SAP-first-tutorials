e3e1141f3266e3f563f642c8066f32a998a6c84a
q-sakib <que.sakib@outlook.com>
2025-09-28 15:17:25 +0600

    step 6

--- sap-inventory/webapp/controller/App.controller.ts
+++ sap-inventory/webapp/controller/App.controller.ts
@@ -1,8 +1,9 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import MessageToast from "sap/m/MessageToast";
 
 export default class App extends Controller { 
   onShowHello():void {
     // show a native JavaScript alert
-    alert("Hello World");
+    MessageToast.show("Hello World");
   }
 }
