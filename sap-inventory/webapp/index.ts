97f4c3a52290023cad5832c63af87b1ee908bd39
q-sakib <que.sakib@outlook.com>
2025-09-28 15:03:00 +0600

    sap ui 5 step 5

--- sap-inventory/webapp/index.ts
+++ sap-inventory/webapp/index.ts
@@ -1,5 +1,7 @@
-import Text from "sap/m/Text";
+import XMLView from "sap/ui/core/mvc/XMLView";
 
-new Text({
-  text: "Hello World TypeScript test"
-}).placeAt("content");
+XMLView.create({
+  viewName: "ui5.walkthrough.view.App"
+}).then(function(view) {
+  view.placeAt("content");
+});
