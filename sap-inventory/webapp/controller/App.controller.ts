f80da612be0dc9d52b03be3e6220212f19fb02cf
q-sakib <que.sakib@outlook.com>
2025-09-28 18:46:29 +0600

    9 comp config

--- sap-inventory/webapp/controller/App.controller.ts
+++ sap-inventory/webapp/controller/App.controller.ts
@@ -2,6 +2,10 @@ import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
 import JSONModel from "sap/ui/model/json/JSONModel";
 
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
+
+
 export default class App extends Controller { 
   onInit(): void  {
     // set data model on view
@@ -12,12 +16,24 @@ export default class App extends Controller {
     };
     const oModel = new JSONModel(oData);
     this.getView()?.setModel(oModel);
+
+
+    const i18nModel = new ResourceModel({
+      bundleName: "ui5.walkthrough.i18n.i18n"
+    });
+    this.getView()?.setModel(i18nModel, "i18n");
   }
 
 
 
-  onShowHello():void {
+  async onShowHello():Promise<void> {
     // show a native JavaScript alert
-    MessageToast.show("Hello " + this.getView()?.getModel()?.getProperty("/recipient/name"));
+    // MessageToast.show("Hello " + this.getView()?.getModel()?.getProperty("/recipient/name"));
+
+    const recipient = (this.getView()?.getModel() as JSONModel).getProperty("/recipient/name");
+    const i18nModel = this.getView()?.getModel("i18n") as ResourceModel;
+    const i18nBundle = await i18nModel.getResourceBundle();
+    const sHelloMsg = i18nBundle.getText("helloMsg", [recipient]) || "Hello";
+    MessageToast.show(sHelloMsg);
   }
 }
--- sap-inventory/webapp/index.ts
+++ sap-inventory/webapp/index.ts
@@ -1,4 +1,15 @@
 import XMLView from "sap/ui/core/mvc/XMLView";
+import ComponentContainer from "sap/ui/core/ComponentContainer";
+
+new ComponentContainer({
+  id: "container",
+  name: "ui5.walkthrough",
+  settings: {
+    id: "walkthrough",
+  },
+  autoPrefixId: true,
+  async: true,
+}).placeAt("content");
 
 XMLView.create({
   viewName: "ui5.walkthrough.view.App"
