14d69045e285635075d9949b8e165c01dce39e56
q-sakib <que.sakib@outlook.com>
2025-09-28 15:20:20 +0600

    5

--- sap-inventory/webapp/controller/App.controller.ts
+++ sap-inventory/webapp/controller/App.controller.ts
@@ -1,7 +1,21 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import MessageToast from "sap/m/MessageToast";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 export default class App extends Controller { 
+  onInit(): void  {
+    // set data model on view
+    const oData = {
+      recipient: {
+        name: "World"
+      }
+    };
+    const oModel = new JSONModel(oData);
+    this.getView()?.setModel(oModel);
+  }
+
+
+
   onShowHello():void {
     // show a native JavaScript alert
     MessageToast.show("Hello World");
